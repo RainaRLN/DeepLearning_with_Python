a705c5655b5f8133a17d53723e65eb48f354da6c
RainaRLN <raina.rao@outlook.com>
2020-06-09 11:08:32 +0800

    Add smooth_curve

--- optimizer_compare_mnist.py
+++ optimizer_compare_mnist.py
@@ -11,6 +11,14 @@ from optimizer import SGD, Momentum, AdaGrad, Adam
 from multi_layer_net import MultiLayerNet
 
 
+def smooth_curve(x):
+    window_len = 11
+    s = np.r_[x[window_len-1:0:-1], x, x[-2:-window_len-1:-1]]
+    w = np.kaiser(window_len, 2)  # 凯泽窗
+    y = np.convolve(w/w.sum(), s, mode='valid') # 卷积
+    return y[window_len//2: len(y)-window_len//2]
+
+
 if __name__ == "__main__":
     #  读取 mnist 数据
     (train_img, train_label), (test_img, test_label) = load_mnist(normalize=True)
@@ -46,27 +54,22 @@ if __name__ == "__main__":
             optimizers[key].update(networks[key].params, grads)
             
             # 计算 loss
-            # loss = networks[key].loss(train_img_batch, train_label_batch)
-            # train_loss[key].append(loss)
+            loss = networks[key].loss(train_img_batch, train_label_batch)
+            train_loss[key].append(loss)
 
         # 每 100 次打印 1 次 loss
         if i % 100 == 0:
             print( "=========== " + "iteration: " + str(i) + " ===========")
             for key in optimizers.keys():
-                loss = networks[key].loss(train_img_batch, train_label_batch)
-                train_loss[key].append(loss)
-                print(key + ": " + str(loss))
+                print(key + ": " + str(train_loss[key][-1]))
 
     # 绘制曲线
     markers = {"SGD": "o", "Momentum": "x", "AdaGrad": "s", "Adam": "D"}
-    x = np.arange(iters_times/100)
+    x = np.arange(iters_times)
     for key in optimizers.keys():
-        # TODO smooth curve
-        plt.plot(x, train_loss[key], marker=markers[key], markevery=10, label=key)
+        plt.plot(x, smooth_curve(train_loss[key]), marker=markers[key], markevery=100, label=key)
     plt.xlabel("iterations")
     plt.ylabel("loss")
     plt.ylim(0, 1)
     plt.legend()
     plt.show()
-
-        
